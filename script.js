070d09bbd02470579bf4b2d6e4199f524c51cc46
Adrian Perez <adrian1804perez@gmail.com>
2022-08-24 12:09:15 -0700

    Add operate function

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ function multiply (num1,num2) {
 function divide (num1,num2) {
    return num1 / num2;
 };
+
+function operate (string) {
+   const array = string.split(" ");
+   const firstNum = +array[0];
+   const secondNum = +array[2];
+   switch (array[1]) {
+      case "+":
+         return add(firstNum,secondNum)
+      case "-":
+         return subtract(firstNum,secondNum);
+      case "*":
+         return multiply(firstNum,secondNum);
+      case "/":
+         return divide(firstNum,secondNum);
+   }
+}
